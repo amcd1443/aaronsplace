707baf7f64074b3846f143958b84d36cdc9fa9d1
Austin Duer <mcclainduer@gmail.com>
2017-08-09 20:38:08 -0400

    added new functions, and to-do list

--- js/main.js
+++ js/main.js
@@ -1,28 +1,54 @@
 $(function () {
   console.log("hello world");
 
-  	var client = contentful.createClient({
+  var client = contentful.createClient({
   		space: '29x3cj9t96dt',
   		accessToken: 'd4f626edbc7cd7ecb2982bbd6ca969a9c186d994138f4e422b3841f5fa237678'
-	})
+	 })
+
+
+  var showDistrictPlaces = function(whichBurrow) {
+    $("#placeList").empty();
+    for (i = 0; i < placesFromContentful.length; i++) {
+      var place = placesFromContentful[i];
+      var placeName = "<li>"+ place.fields.placename + " - " + place.fields.placedescription + "</li>";
+      if (place.fields.placelocation == whichBurrow) {
+        $("#placeList").append(placeName);  
+      }
+    }
+    console.log(whichBurrow + " button was clicked");
+  };
+
+  var placesFromContentful 
+
+  $("#manhattanButton").click(function() {
+    showDistrictPlaces("Manhattan");
+  });
+
+  $("#brooklynButton").click(function() {
+    showDistrictPlaces("Brooklyn");
+  });
+
+  $("#eastVillageButton").click(function() {
+    showDistrictPlaces("East Village");
+  });
+
+  $("#flatIronGramercyParkButton").click(function() {
+    showDistrictPlaces("flat iron gramercy");
+  })
+
+
 
 	client.getEntries().then(function (entries) {
   		var places = entries.items;
   		for (i = 0; i < places.length; i++) {
-  			var place = places[i];
-
-  			var placeName = "<li>"+ place.fields.placename + " - " + place.fields.placedescription + "</li>";
-  			$("#manhattanplaces").append(placeName);
-			
-  			console.log(place.fields.placelocation);
-
-
-
-
-		}
-
-
+  		  var place = places[i];
+  		  var placeName = "<li>"+ place.fields.placename + " - " + place.fields.placedescription + "</li>";
+  		  $("#placeList").append(placeName);
+		  }
+    placesFromContentful = places;
 	});
+
 
 
 
@@ -30,6 +56,6 @@ $(function () {
 });
 
 
-// homework: continue making website
-// make it filter for place.fields.placelocation
+// finish website 
+// then do new html5 boiler plate, and start a new website, git repo, and then start again. 
 
